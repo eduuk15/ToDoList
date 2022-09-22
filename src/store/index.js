d7c018c045870f483952018d773ab40a5b6b7391
Eduardo <eduardo@conectra.com.br>
2022-09-22 18:42:32 -0300

    feat: adiciona funcionalidade de alteração de status da tarefa

--- src/store/index.js
+++ src/store/index.js
@@ -26,6 +26,9 @@ export default createStore({
       return axios.post('http://localhost:3000/todos', data).then((response) => {
         commit('storeTodo', response.data);
       })
+    },
+    updateTodo(context, { id, data}) {
+      return axios.put(`http://localhost:3000/todos/${id}`, data)
     }
   },
   modules: {
