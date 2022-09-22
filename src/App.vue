830356978d81551e1beb393bd1f17a27bacb99fc
Eduardo <eduardo@conectra.com.br>
2022-09-22 19:27:33 -0300

    feat: adiciona titulo (falta estilizacao)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="px-3 py-10 md:px-10">
         <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
+          <div class="flex justify-center">
+              To Do List (Lista de Tarefas)
+          </div>
 
           <TodoSpinner v-if="loading"/>
 
