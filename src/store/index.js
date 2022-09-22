c1a5924377bb317e258380d7bc7db958de847f3b
Eduardo <eduardo@conectra.com.br>
2022-09-22 18:54:01 -0300

    feat: sincroniza title com vuex

--- src/store/index.js
+++ src/store/index.js
@@ -12,7 +12,12 @@ export default createStore({
       state.todos = payload
     },
     storeTodo(state, payload) {
-      state.todos.unshift(payload)
+      const index = state.todos.findIndex(todo => todo.id === payload.id)
+      if (index >= 0) {
+        state.todos.splice(index, 1, payload)
+      } else {
+        state.todos.unshift(payload)
+      }
     },
   },
   actions: {
@@ -27,8 +32,10 @@ export default createStore({
         commit('storeTodo', response.data);
       })
     },
-    updateTodo(context, { id, data}) {
-      return axios.put(`http://localhost:3000/todos/${id}`, data)
+    updateTodo({ commit }, { id, data}) {
+      return axios.put(`http://localhost:3000/todos/${id}`, data).then((response) => {
+      commit('storeTodo', response.data)
+      })
     }
   },
   modules: {
